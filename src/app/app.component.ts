492014410ac36b9ccdfa87a0082063943d43cdf2
Csaba Todor <todorcs@gmail.com>
2016-10-26 17:27:34 -0500

    Updated App Component and its HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'zt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'zt works!';
+
+  @Input() priceMinFilter: number;
+  @Input() priceMaxFilter: number;
+
+  filterPrice(filter) {
+    this.priceMinFilter = filter.priceMin;
+    this.priceMaxFilter = filter.priceMax;
+  }
+
+  _productPrices = [
+    {
+      "name": "Product One",
+      "price": 600,
+    },
+    {
+      "name": "Product Two",
+      "price": 1100,
+    },
+    {
+      "name": "Product Three",
+      "price": 2150,
+    },
+    {
+      "name": "Product Four",
+      "price": 3500,
+    },
+    {
+      "name": "Product Five",
+      "price": 4300,
+    },
+    {
+      "name": "Product Six",
+      "price": 5400,
+    },
+    {
+      "name": "Product Seven",
+      "price": 6900,
+    },
+    {
+      "name": "Product Eighth",
+      "price": 14000,
+    },
+    {
+      "name": "Product Nine",
+      "price": 26000,
+    },
+    {
+      "name": "Product Ten",
+      "price": 30000,
+    },
+    {
+      "name": "Product Eleven",
+      "price": 160000,
+    },
+    {
+      "name": "Product Twelve",
+      "price": 1000000,
+    },
+  ]
 }
